337098e2c6046f57b26f756053e9ffcddb1fb949
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2021-03-09 15:05:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='fscacher',
     version='0.2.0',
     py_modules=['fscacher'],
-    url='',
+    url='https://github.com/pnsaevik/fscacher',
     license='MIT',
     author='Pål Næverlid Sævik',
-    author_email='',
-    description=''
+    author_email='pnsaevik@gmail.com',
+    description='File system based caching'
 )
