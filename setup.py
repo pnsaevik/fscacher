d638d7d55cafd95de355ae258642c4b2e5f17d71
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2021-03-10 07:01:40 +0100

    Add pypi metadata

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
-from setuptools import setup
+#!/usr/bin/env python
 
-setup(
-    name='fscacher',
-    version='0.2.0',
-    py_modules=['fscacher'],
-    url='https://github.com/pnsaevik/fscacher',
-    license='MIT',
-    author='Pål Næverlid Sævik',
-    author_email='pnsaevik@gmail.com',
-    description='File system based caching'
-)
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
